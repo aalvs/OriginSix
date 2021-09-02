331af15890b915a3ffef7aa27c36197e6e3c320a
Andre Alves <andrealvesdeoliveira@outlook.com>
2021-09-02 20:03:30 -0300

    Concluído contato e implementado ScrollReveal.

--- src/script.js
+++ src/script.js
@@ -44,3 +44,23 @@ const swiper = new Swiper('.swiper', {
       }
     }
   })
+
+  /* ScrollReveal: Mostrar elementos quando der scroll na página */
+const scrollReveal = ScrollReveal({
+    origin: 'top',
+    distance: '30px',
+    duration: 700,
+    reset: true
+})
+
+scrollReveal.reveal(
+    `#home .image, #home .text,
+    #about .image, #about .text,
+    #services header, #services .card,
+    #testimonials header, #testimonials .testimonials
+    #contact .text, #contact .links,
+    footer .brand, footer .social
+    `,
+    { interval: 100 }
+)
+  
