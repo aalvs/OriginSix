fe7a498c871e4e28dab43433c0bf0445e39b412d
Andre Alves <andrealvesdeoliveira@outlook.com>
2021-09-01 22:37:46 -0300

    Concluída a estilização da seção de serviços.

--- src/script.js
+++ src/script.js
@@ -16,3 +16,15 @@ for(const link of links) {
         nav.classList.remove('show')
     })
 }
+
+// Colocar sombra no header quando o scroll for > que a altura do header
+const header = document.querySelector('#header');
+const navHeight = header.offsetHeight;
+
+window.addEventListener('scroll', function() {
+    if(window.scrollY >= navHeight) {
+        header.classList.add('scroll')
+    } else {
+        header.classList.remove('scroll')
+    }
+})
